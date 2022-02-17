b6e7be7438b6953de6a0684e25f75a444463ae50
GooieJS <conradklek@gmail.com>
2022-02-17 01:08:17 -0600

    submit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getDataFromApi = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=3qgy7RNMMouhfSQAkdQJCEkyGWQOeTEiX6Jl2ZV6"
+      )
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <button onClick={getDataFromApi}>Get Data</button>
+      <div>
+        <img src={data.url} alt={data.title} />
+        <main>
+          <h1>{data.title}</h1>
+          <p>{data.explanation}</p>
+        </main>
+      </div>
     </div>
   );
 }
